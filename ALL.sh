83c78ec3bd61c8c3dc252442de997ed8dd0a2b02
tamirof <121957078+tamirof@users.noreply.github.com>
2023-01-12 11:17:31 +0200

    Update and rename logfolder.sh to ALL.sh

---
+++ ALL.sh
@@ -0,0 +1,13 @@
+echo "Selected ALL Language, python, java, c"
+#if [ $1 == 'ALL' ]
+#then
+
+#mkdir "$foldername"
+#ls -la
+#cd "$foldername"
+#pwd
+#touch "$filename"
+#ls -la
+#else
+#echo " folder exists"
+#fi
