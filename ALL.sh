f1f35c4ef948dcd2284ee5742028ac1e45297632
tamirof <121957078+tamirof@users.noreply.github.com>
2023-01-23 18:24:39 +0200

    Update ALL.sh

--- ALL.sh
+++ ALL.sh
@@ -1,13 +1,1 @@
-echo "Selected ALL Language, python, java, c"
-#if [ $1 == 'ALL' ]
-#then
-
-#mkdir "$foldername"
-#ls -la
-#cd "$foldername"
-#pwd
-#touch "$filename"
-#ls -la
-#else
-#echo " folder exists"
-#fi
+echo "You have Selected ALL Languages, python, java, c"
